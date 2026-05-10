3c8e48565964c2645d26d00d88d61eb37de5827e
Ali-Shehdy <E4337126@live.tees.ac.uk>
2026-05-10 01:25:02 +0100

    Using async/await with EF Core is the correct modern backend approach

--- obj/Debug/net10.0/UserManegment.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManegment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManegment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69d1cc5e0048becc0a7c437199b6d3699804c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ce4e7d515401e3dc2cc6989ada9e207317522f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManegment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManegment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
